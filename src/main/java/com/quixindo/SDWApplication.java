0da84ba68d5914571d7d4cc1b041df73009faaa3
QuixindoDev <eufrasio.dev@gmail.com>
2024-09-08 08:00:55 +0100

    first commit

---
+++ src/main/java/com/quixindo/SDWApplication.java
@@ -0,0 +1,13 @@
+package com.quixindo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SDWApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SDWApplication.class, args);
+	}
+
+}
